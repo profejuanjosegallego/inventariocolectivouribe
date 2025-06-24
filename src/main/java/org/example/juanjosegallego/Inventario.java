d7aa71812e27663da75af10a2e5c95d0769e777e
SebastianGomezdv <juansebastian_gomez456@hotmail.com>
2025-06-24 15:17:46 -0500

    feat se crea opciones 1 y 2 del menu

--- src/main/java/org/example/juanjosegallego/Inventario.java
+++ src/main/java/org/example/juanjosegallego/Inventario.java
@@ -1,5 +1,7 @@
 package org.example.juanjosegallego;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Inventario {
@@ -9,6 +11,7 @@ public class Inventario {
         Scanner keyEntry = new Scanner(System.in);
         Integer menuOption=null;
         String colorVerde="\u001B[32m";
+        ArrayList<Object> Products=new ArrayList<>();
 
 
         //PROCESO 1 (Crear el menu de opciones)
@@ -24,8 +27,58 @@ public class Inventario {
         System.out.println("4️⃣  Eliminar una prenda del sistema 🗑️");
         System.out.println("5️⃣  Presiona 5 para SALIR ❌");
 
-        while(menuOption!=5){
-            System.out.println("oe");
+        System.out.println("Digite una opcion: ");
+        menuOption=keyEntry.nextInt();
+
+        while(true){
+            HashMap<String, Object> diccionario= new HashMap<>();
+
+            try {
+                if (menuOption==1) {
+                    System.out.println("Registrando un producto...");
+
+                    //Agregando un elemento a una lista de Java
+                   // System.out.println("Digita el producto que quieres agregar a la BD");
+                   // keyEntry.nextLine();
+                   // Products.add(keyEntry.nextLine());//
+
+                    // creando objetos/diccionarios/hashmaps en java
+
+
+                    //llenando un diccionario
+                    System.out.println("Digita el ID del producto: ");
+                    diccionario.put("id",keyEntry.nextInt());
+
+                    System.out.println("Digita el Precio del producto: ");
+                    diccionario.put("precio", keyEntry.nextInt());
+
+                    System.out.println("Digita el Nombre del producto: ");
+                    keyEntry.nextLine(); //limpio el bufer
+                    diccionario.put("nombre", keyEntry.nextLine());
+                    // llenando una lista con un diccionario
+                    Products.add(diccionario);
+
+                    System.out.println(Products);
+
+                    } else if (menuOption==2) {
+                    System.out.println("Mostrando el inventario...");
+                    System.out.println(Products);
+
+                    } else if (menuOption==3) {
+                    System.out.println("Modificando un producto...");
+                    } else if (menuOption==4) {
+                    System.out.println("Eliminando un producto del inventario");
+                    } else if (menuOption==5) {
+                    System.out.println("Gracias por utilizar nuestra APP...");
+                    break;
+                    }
+
+                System.out.println("Ahora digita otra opción: ");
+                menuOption=keyEntry.nextInt();
+
+                }catch (Exception error){
+                System.out.println(error.getMessage());
+            }
         }
 
         //PROCESO 2
