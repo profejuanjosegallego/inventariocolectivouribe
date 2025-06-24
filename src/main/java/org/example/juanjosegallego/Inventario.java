4f1da20ea74fd7fd4b70b8d5bbbc5f460d6ff13a
Lian0720 <159823803+Lian0720@users.noreply.github.com>
2025-06-24 15:34:49 -0500

    feat se configura opción 1 y opción 2

--- src/main/java/org/example/juanjosegallego/Inventario.java
+++ src/main/java/org/example/juanjosegallego/Inventario.java
@@ -2,7 +2,9 @@ package org.example.juanjosegallego;
 
 import java.nio.channels.ScatteringByteChannel;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
+
 
 public class Inventario {
     public static void main(String[] args) {
@@ -11,7 +13,7 @@ public class Inventario {
         Scanner keyEntry = new Scanner(System.in);
         Integer menuOption=null;
         String colorVerde="\u001B[32m";
-
+        ArrayList<Object> productos = new ArrayList<>();
 
         //PROCESO 1 (Crear el menu de opciones)
         System.out.println("\n******************");
@@ -28,30 +30,53 @@ public class Inventario {
 
         System.out.println("Apreciada gonorrea , digita una opción");
         menuOption = keyEntry.nextInt();
-        while(menuOption!=5){
+        while(menuOption!=5) {
+            HashMap<String, Object> diccionary = new HashMap<>();
             try {
-                if(menuOption==1){
-                    ArrayList  <String> productos = new ArrayList();;
-                }else if(menuOption==2){
 
-                }else if(menuOption==3){
+                if (menuOption == 1) {
+                    System.out.println("Registrando  producto...");
+                    //System.out.println("Digite el producto a1 registrar en la Base de Datos");
+                    //keyEntry.nextLine();
+                   // productos.add(keyEntry.nextLine());
 
-                }else if(menuOption==4){
+                    //creando ojbetos/diccionarios/ hashmaps en js
 
-                }else if(menuOption==5){
+                    //llenando un diccionario/objeto o hashmap
+                    System.out.println("Digita el id del producto: ");
+                    diccionary.put("id", keyEntry.nextInt());
 
+                    System.out.println("Digita  el precio del producto: ");;
+                    diccionary.put("precio", keyEntry.nextDouble());
+
+                    System.out.println("Digita el nombre del producto: ");
+                    keyEntry.nextLine(); //limpio el buffer
+                   diccionary.put("nombre", keyEntry.nextLine());
+
+                   //llenando lista con diccionario
+                    productos.add(diccionary);
+
+
+                    System.out.println(diccionary);
+                } else if (menuOption == 2) {
+                    System.out.println("Mostrando el inventario...");
+                    System.out.println(productos);
+                } else if (menuOption == 3) {
+                    System.out.println("Modificando producto....");
+                } else if (menuOption == 4) {
+                    System.out.println("Eliminando producto....");
+                } else if (menuOption == 5) {
+                    System.out.println("Gracias por usar nuestro software...");
+                    break;
                 }
-            }
-            catch(Exception error) {
-                System.out.println("error, opción no valida, solo 1 a 5");
+                System.out.println("Ahora digita otra opción");
+                menuOption = keyEntry.nextInt();
+            } catch (Exception error) {
+                System.out.println(error.getMessage());
             }
         }
-
         //PROCESO 2
-
         //PROCESO 3
-
         //SALIDAS
-
     }
 }
