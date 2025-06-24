3fdf0ac90c36a39e1839ace664a64403c87e9272
CesdeUser <EstudianteCesde@cesde.net>
2025-06-24 15:15:52 -0500

    ArrayList y HashMap

--- src/main/java/org/example/BraihanLopera/VectoresyArrays.java
+++ src/main/java/org/example/BraihanLopera/VectoresyArrays.java
@@ -1,35 +1,41 @@
 package org.example.BraihanLopera;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
 import java.util.Scanner;
 public class VectoresyArrays {
     public static void main(String[] arg){
         Scanner sc = new Scanner(System.in);
+        /*Colores*/
         String Rojo = "\u001B[31m";
+        String Verde = "\u001B[32m";
         String Reset = "\u001B[0m";
-        Integer OpcionMenu = null;
-        System.out.println(Rojo + "texto rojo" + Reset);
-        System.out.print("1 para ver el menu:");
-        OpcionMenu = sc.nextInt();
 
-        while(OpcionMenu != 5){
-            try{
-                //Evaluar las condiciones del menu
-                System.out.println("Opcion 1");
-                System.out.println("Opcion 2");
-                System.out.println("Opcion 3");
-                System.out.print("Tu eleccion:");
-                OpcionMenu = sc.nextInt();
-                if(OpcionMenu == 1){
-                    System.out.println("Elegiste la opcion 1");
-                }else if (OpcionMenu == 2){
-                    System.out.println("Elegiste la ocpion 2");
-                }else if(OpcionMenu == 3){
-                    System.out.println("Elegiste la opcion 3");
-                }
-            }catch(Exception error){
-                System.out.println("Opcion incorrecta");
-            }
+        /*ArrayList*/
+        ArrayList<HashMap<String,Object>> productos = new ArrayList<>();
 
-        }
+        /*diccionario/objeto7hasmaps en java*/
+        HashMap<String, Object> diccionary = new HashMap<>(); /*String: todas las entradas seran palabras Integer:Los valores seran enteros*/
+
+        //llenando un diccionario
+        System.out.print("Id del producto:");
+        diccionary.put("ID", sc.nextInt());
+        sc.nextLine();
+        System.out.print("Nombre del producto: ");
+        diccionary.put("Nombre del producto:", sc.nextLine());
+        System.out.print("Por ultimo, el precio del producto: ");
+        diccionary.put("Precio", sc.nextLine());
+        System.out.print("Listo, tu producto es: " + diccionary);
+        productos.add(diccionary);
+        System.out.println(productos);
+
+
+
+
+
+
+
+
     }
 }
